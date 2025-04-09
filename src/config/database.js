89000669a678e7b3819965884afbda3097afaaf2
Savitha <gsavitha@protonmail.com>
2025-04-10 01:00:06 +0530

    add env variable

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA } = process.env
 
 const sequelize = new Sequelize(
   DB_NAME,
@@ -13,7 +13,7 @@ const sequelize = new Sequelize(
   port: DB_PORT,
   dialect: 'postgres',
   logging: false,
-  schema: 'coffee_shop_schema'
+  schema: DB_SCHEMA,
 }
 )
 
